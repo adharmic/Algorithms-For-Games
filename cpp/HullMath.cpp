584bc3aa1049dcb03a3a687faef58d14f176d30a
Adithya Ajith <50308085+adithyajith925@users.noreply.github.com>
2021-09-26 13:27:15 -0400

    Method headers now include brief programming guide

--- cpp/HullMath.cpp
+++ cpp/HullMath.cpp
@@ -1,6 +1,7 @@
 #include <d2d1.h>
 
 #include <vector>
+#include "Vector2D.h"
 using namespace std;
 
 class HullMath {
@@ -35,12 +36,24 @@ class HullMath {
 
 	/* ContainsPoint will receive a point and determine if it is inside the given hull. 
 	The algorithm will likely be borrowed from prof. McKenna's AFG.
+
+	Translating McKenna et al.'s algorithm is proving to be harder than initially thought. We will do this another way:
+
+	1. Since we are coming into this with a convex hull already computed, we can use vectors to determine our answer.
+	2. Go through each edge of the hull counterclockwise. 
+	3. For each edge, check to see if the point in question is to the LEFT of the edge.
+	4. If it is not for any edge, the point is NOT in the hull.
+	5. If it makes a full round with no issues, the point must be in the hull.
 	*/
 	static bool ContainsPoint(vector<D2D1_ELLIPSE> hull, D2D1_ELLIPSE point) {
 		return false;
 	}
 
-	/* HullsIntersecting will probably check if any of the points of hull1 are inside hull2. */
+	/* HullsIntersecting will probably check if any of the points of hull1 are inside hull2. 
+	*  i.e. Loop through all the points of hull1, and for each point, check if it is inside hull2.
+	*  Do the same for hull2 and hull1. 
+	*  By definition, they cannot intersect without one of the points being inside the other hull.
+	*/
 	static bool HullsIntersecting(vector<D2D1_ELLIPSE> hull1, vector<D2D1_ELLIPSE> hull2) {
 		return false;
 	}
